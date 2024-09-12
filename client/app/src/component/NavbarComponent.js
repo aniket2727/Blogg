4bb54deac0987f77b725a4d509e04b93c2494b6c
aniket2727 <aniketkadam5950@gmail.com>
2024-09-12 12:50:10 +0530

    add code

--- client/app/src/component/NavbarComponent.js
+++ client/app/src/component/NavbarComponent.js
@@ -1,11 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
+import { useCallback } from 'react';
 import ButtonComponent from './ButtonComponent';
-
+import debounce from 'lodash/debounce';
 const NavbarComponent = () => {
 
-    const handleButtonClick=()=>{
-           console.log("button get clicked")
-    }
+    
+    const handleButtonClick = useCallback(debounce(() => {
+        console.log("Button was clicked");
+    }, 300), []); 
   return (
     <div className="bg-gray-800 text-white">
       <div className="p-4">
@@ -15,7 +18,7 @@ const NavbarComponent = () => {
       <div className="p-4 flex items-center  space-x-4">
         <ButtonComponent buttonText="Login" backgroundColorprop="bg-red-500" onClick={handleButtonClick} /> 
         <ButtonComponent buttonText="Register" backgroundColorprop="bg-red-500" onClick={handleButtonClick} />
-        <ButtonComponent buttonText="Logout" backgroundColorprop="bg-red-500" onClick={handleButtonClick} /> {/* Example of custom button styling */}
+        <ButtonComponent buttonText="Logout" backgroundColorprop="bg-red-500" onClick={handleButtonClick} /> 
       </div>
     </div>
   );
