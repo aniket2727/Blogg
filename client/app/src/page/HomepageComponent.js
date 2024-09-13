f8b7d93d0de4eda43f630787bc0ca1d3ad4501a7
aniket2727 <aniketkadam5950@gmail.com>
2024-09-14 00:47:26 +0530

    add code

--- client/app/src/page/HomepageComponent.js
+++ client/app/src/page/HomepageComponent.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 import './homepage.css';
-import homepageImage from '../static File/homepage-image.jpg'; 
+import homepageImage from '../static File/homepage-image.jpg';
+
 const HomepageComponent = () => {
   return (
     <div className='main-parent-div'>
       <div className='left-side-div'>
         <img src={homepageImage} alt='Home Page' />
       </div>
-       
+
       <div className='right-side-div'>
-        <h1>This is context div</h1>
+        <div className='title'>
+          <h1 className='title1'>Welcome to Corporate Kattta</h1>
+        </div>
+        <div className='words-move-animation-container'>
+          <div className='typewriter-container'>
+            <Typewriter
+              options={{
+                strings: [
+                  'Connect with your buddies\nand corporate friends',
+                  'Chat, post sharing\nand much more'
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 50, // Adjust typing speed
+                deleteSpeed: 30, // Adjust deletion speed if needed
+                cursor: '' // Remove the cursor if you don't want it to display
+              }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
