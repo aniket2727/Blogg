4abbae36154e06058136d04dbc0a3264b5ee764c
aniket2727 <aniketkadam5950@gmail.com>
2024-09-26 14:14:00 +0530

    add all code

--- server/router/followerRouter.js
+++ server/router/followerRouter.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { Addusersintoafollowerslist, Removeuserfromfollowerslist } = require('../controller/followersManagementControlller');
+const { 
+    Addusersintoafollowerslist, 
+    Removeuserfromfollowerslist, 
+    GetFollowerCount, 
+    GetFollowersWithUserInfo 
+} = require('../controller/followersManagementControlller');
 
 // Routes
-router.post('/addfollower', Addusersintoafollowerslist);
-router.post('/removefollower', Removeuserfromfollowerslist);
+router.post('/addfollower', Addusersintoafollowerslist); // Add follower
+router.post('/removefollower', Removeuserfromfollowerslist); // Remove follower
+router.get('/getfollowerscount/:userid', GetFollowerCount); // Get follower count
+router.get('/getfollowersuserinfo/:userid', GetFollowersWithUserInfo); // Get followers info
 
 // Export router
 module.exports = router;
